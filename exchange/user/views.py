89439c2ffc4031bea54511e7ebea9648f41e5798
Lijo <lijo.belardi@icloud.com>
2021-12-01 13:52:01 +0100

    Added decorator and sorted the imported modules in user/views.py

--- exchange/user/views.py
+++ exchange/user/views.py
@@ -1,8 +1,9 @@
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 from .forms import NewUserForm
-from django.contrib.auth import login, authenticate, logout
-from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm
 
 def register_request(request):
     if request.method == "POST":
@@ -39,5 +40,6 @@ def logout_request(request):
     messages.info(request, "You have successfully logged out.")
     return redirect("user:login")
 
+@login_required()
 def profile(request):
     return render(request, template_name="user/profile.html")
