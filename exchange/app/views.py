724cf7002b648da5c1b1d2030a200c9a79a5eb5f
Lijo <lijo.belardi@icloud.com>
2021-12-02 18:58:28 +0100

    Started function "publish_order_to_buy"

--- exchange/app/views.py
+++ exchange/app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .forms import PublishOrderToBuy
 
 @login_required()
 def homepage_view(request):
     return render(request, "homepage.html")
+
+def publish_order_to_buy(request):
+    ...
