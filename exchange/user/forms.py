52ddbd9d592b879990f67c283e59f1f3aed68f93
Lijo <lijo.belardi@icloud.com>
2021-12-03 20:06:30 +0100

    Added first name and last name sections.

--- exchange/user/forms.py
+++ exchange/user/forms.py
@@ -2,12 +2,15 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+
 class NewUserForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ("username", "first_name", "last_name", "email", "password1", "password2")
 
     def save(self, commit=True):
         user = super(NewUserForm, self).save(commit=False)
