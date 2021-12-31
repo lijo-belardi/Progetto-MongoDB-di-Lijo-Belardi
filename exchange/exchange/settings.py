8a0538bd5279bc7d7c90d7483030962c2b1503ff
Lijo <lijo.belardi@icloud.com>
2021-12-31 16:41:10 +0100

    Various changes

--- exchange/exchange/settings.py
+++ exchange/exchange/settings.py
@@ -31,15 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app',
+    'user',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
-    'user',
-    'crispy_forms',
 ]
 
 MIDDLEWARE = [
